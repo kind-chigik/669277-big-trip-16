489d39e4519938782158e8b9ed9b9e389a8853c1
Инна Русина <rus.work89@yandex.ru>
2021-11-25 21:45:53 +0300

    Добавлен модуль для отрисовки элементов

---
+++ src/render.js
@@ -0,0 +1,10 @@
+export const renderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+export const renderTemplate = (element, template, place) => {
+  element.insertAdjacentHTML(place, template);
+};
