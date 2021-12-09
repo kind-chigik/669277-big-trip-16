1c38101dbd3bfbb5d49018ec1d0f5cbf66dffa63
Инна Русина <rus.work89@yandex.ru>
2021-12-09 21:58:44 +0300

    Скорректированы функции отрисовки элементов

--- src/render.js
+++ src/render.js
@@ -1,10 +1,32 @@
-export const renderPosition = {
+const renderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
   BEFOREEND: 'beforeend',
   AFTEREND: 'afterend',
 };
 
-export const renderTemplate = (element, template, place) => {
-  element.insertAdjacentHTML(place, template);
+const renderElement = (container, element, place) => {
+  switch (place) {
+    case renderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case renderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case renderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case renderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
 };
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export {renderPosition, renderElement, createElement};
