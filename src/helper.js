3a66cb7b5722b61ecf7cf0aa41b962fac1727d38
Инна Русина <rus.work89@yandex.ru>
2021-12-17 21:14:16 +0300

    Добавлена функция для обновления элемента массива

--- src/helper.js
+++ src/helper.js
@@ -2,3 +2,18 @@ const KEY_ESCAPE = 'Escape';
 const KEY_ESC = 'Esc';
 
 export const isKeyEsс = (evt) => evt.key === KEY_ESCAPE || evt.key === KEY_ESC;
+
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
