4f97af66746df7cca0606254ecd0f405486cac15
Инна Русина <rus.work89@yandex.ru>
2021-12-17 21:16:53 +0300

    Логика отрисовки точек маршрута перенесена в презентеры
    Теперь отрисовкой точек маршрута занимается tripPresenter

--- src/main.js
+++ src/main.js
@@ -1,55 +1,12 @@
 import {renderPosition, renderElement} from './render.js';
 import MenuView from './view/menu-view.js';
 import FilterView from './view/filters-view.js';
-import ContentView from './view/content-view.js';
-import SortView from './view/sort-view.js';
-import PointView from './view/point-view.js';
-import FormPointEditView from './view/form-point-edit-view.js';
-import NoPoints from './view/no-point-view.js';
 import {generatePoint} from './mock/point.js';
-import {isKeyEsс} from './helper.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
 const POINT_COUNT = 15;
 
 const points = Array.from({length: POINT_COUNT}, generatePoint);
-
-const renderPoints = (elementPlace, point) => {
-  const pointComponent = new PointView(point);
-  const pointEditComponent = new FormPointEditView(point);
-
-  const replacePointToForm = () => {
-    elementPlace.replaceChild(pointEditComponent.element, pointComponent.element);
-  };
-
-  const replaceFormToPoint = () => {
-    elementPlace.replaceChild(pointComponent.element, pointEditComponent.element);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (isKeyEsс) {
-      evt.preventDefault();
-      replaceFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
-
-  pointComponent.setListenerClickEdit(() => {
-    replacePointToForm();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  pointEditComponent.setListenerSubmit(() => {
-    replaceFormToPoint();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
-
-  pointEditComponent.setListenerClickClose(() => {
-    replaceFormToPoint();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
-
-  return renderElement(elementPlace, pointComponent, renderPosition.BEFOREEND);
-};
 
 const navigation = document.querySelector('.trip-controls__navigation');
 const filters = document.querySelector('.trip-controls__filters');
@@ -57,22 +14,10 @@ const tripEvents = document.querySelector('.trip-events');
 
 const menuInstance = new MenuView();
 const filterInstance = new FilterView();
-const noPointsInstance = new NoPoints();
-const sortInstance = new SortView();
-const contentInstance = new ContentView();
 
 renderElement(navigation, menuInstance, renderPosition.BEFOREEND);
 renderElement(filters, filterInstance, renderPosition.BEFOREEND);
 
-if (points.length === 0) {
-  renderElement(tripEvents, noPointsInstance, renderPosition.BEFOREEND);
-} else {
-  renderElement(tripEvents, sortInstance, renderPosition.BEFOREEND);
-  renderElement(tripEvents, contentInstance, renderPosition.BEFOREEND);
+const tripPresenter = new TripPresenter(tripEvents);
+tripPresenter.init(points);
 
-  const content = tripEvents.querySelector('.trip-events__list');
-
-  for (let i = 0; i < POINT_COUNT; i++) {
-    renderPoints(content, points[i]);
-  }
-}
