e0fb683c0f7af23a6923c10fa6b65ba06c90daad
Инна Русина <rus.work89@yandex.ru>
2022-01-13 00:31:49 +0300

    Добавлен абстрактный класс для умного компонента

---
+++ src/view/smart-view.js
@@ -0,0 +1,29 @@
+import AbstractView from './abstract-view.js';
+
+class SmartView extends AbstractView {
+  updateElement = () => {
+    const prevElement = this.element;
+    const parentElement = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.element;
+    parentElement.replaceChild(newElement, prevElement);
+  }
+
+  updateData = (update) => {
+    if (!update) {
+      return;
+    }
+
+    this._condiotions = {...this._condiotions, ...update};
+
+    this.updateElement();
+    this.restoreHandlers();
+  }
+
+  restoreHandlers = () => {
+    throw new Error('Абстрактный метод не имплементирован: restoreHandlers');
+  }
+}
+
+export {SmartView as default};
