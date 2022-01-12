38cead303773035ae19409f40462efe48bdd306e
Инна Русина <rus.work89@yandex.ru>
2022-01-13 00:35:23 +0300

    Изменен принцип генерации некоторых свойств, добавлены новые свойства

--- src/mock/point.js
+++ src/mock/point.js
@@ -52,6 +52,15 @@ const getRandomDescription = (array) => {
   return randomDescription;
 };
 
+const getRandomPhotos = () => {
+  const numberPhotos = getRandomNumber(0, 4);
+  const randomPhotos = [];
+  for (let i = 0; i <= numberPhotos; i++) {
+    randomPhotos.push(`http://picsum.photos/248/152?r=${getRandomNumber(0, 5)}`);
+  }
+  return randomPhotos;
+};
+
 const getDateStart = () => {
   const maxDaysGap = 7;
   const daysGap = getRandomNumber(-maxDaysGap, maxDaysGap);
@@ -66,7 +75,7 @@ const getDateEnd = () => {
 };
 
 const getRandomOffers = () => {
-  const randomOffres = [];
+  const randomOffers = [];
 
   let offers = [];
   for (const element of TYPES) {
@@ -78,11 +87,34 @@ const getRandomOffers = () => {
       const checked = Boolean(getRandomNumber());
       offers.push({title, cost, checked});
     }
-    randomOffres.push({type, offers});
+    randomOffers.push({type, offers});
     offers = [];
   }
 
-  return randomOffres;
+  return randomOffers;
+};
+
+const getDestinationForCities = () => {
+  const destinationForCities = [];
+
+  for (const city of CITIES) {
+    let description = '';
+    const countSentence = getRandomNumber(0, 5);
+    for (let i = 0; i < countSentence; i++) {
+      description += getRandomDescription(DESCRIPTIONS);
+    }
+    let photos = [];
+    const countPhotos = getRandomNumber(0, 5);
+    for (let i = 0; i < countPhotos; i++) {
+      photos.push(getRandomPhotos());
+    }
+
+    destinationForCities.push({city, description, photos});
+    description = [];
+    photos = [];
+  }
+
+  return destinationForCities;
 };
 
 const generatePoint = () => {
@@ -95,19 +127,28 @@ const generatePoint = () => {
     }
   });
 
+  const city = getRandomElement(CITIES);
+  const destinationForCities = getDestinationForCities();
+  const destination = {};
+  destinationForCities.forEach((element) => {
+    if (element.city === city) {
+      destination.description = element.description;
+      destination.photos = element.photos;
+    }
+  });
+
   return {
     id: nanoid(),
     type,
-    city: getRandomElement(CITIES),
-    destination: {
-      description: getRandomDescription(DESCRIPTIONS),
-      photo: `http://picsum.photos/248/152?r=${getRandomNumber(0, 5)}`,
-    },
+    city,
+    destination,
     dateStart: getDateStart(),
     dateEnd: getDateEnd(),
     price: getRandomNumber(0, 500000),
     isFavorite: Boolean(getRandomNumber()),
     offers,
+    randomOffers,
+    destinationForCities,
   };
 };
 
