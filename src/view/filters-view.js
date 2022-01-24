74f271c617222a1d1d261d89f6805477ed023902
Инна Русина <rus.work89@yandex.ru>
2022-01-24 23:59:51 +0300

    Скорректированы вьюхи сортировки и фильтра:
    Вью теперь учитывают текущий тип сортировки\фильтра

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,29 +1,50 @@
 import AbstractView from './abstract-view.js';
 
-const createFilters = () => (
+const getTemplateFilters = (filters, currentFilterType) => {
+  const fragment = [];
+  filters.forEach((filter) => {
+    const {type, name} = filter;
+    fragment.push(`<div class="trip-filters__filter">
+    <input id="filter-${name.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name.toLowerCase()}" ${type === currentFilterType ? 'checked' : ''}>
+    <label class="trip-filters__filter-label" for="filter-${name.toLowerCase()}">${name}</label>
+  </div>`);
+  });
+
+  return fragment.join('');
+};
+
+const createFilters = (filters, currentFilterType) => (
   `<form class="trip-filters" action="#" method="get">
-  <div class="trip-filters__filter">
-    <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-    <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-  </div>
+    ${getTemplateFilters(filters, currentFilterType)}
 
-  <div class="trip-filters__filter">
-    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future" disabled>
-    <label class="trip-filters__filter-label" for="filter-future">Future</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" disabled>
-    <label class="trip-filters__filter-label" for="filter-past">Past</label>
-  </div>
-
-  <button class="visually-hidden" type="submit">Accept filter</button>
-</form>`
+    <button class="visually-hidden" type="submit">Accept filter</button>
+  </form>`
 );
 
 class FilterView extends AbstractView {
+  #filters = null;
+  #currentFilterType = null;
+
+  constructor(filters, currentFilterType) {
+    super();
+    this.#filters = filters;
+    this.#currentFilterType = currentFilterType;
+  }
+
   get template() {
-    return createFilters();
+    return createFilters(this.#filters, this.#currentFilterType);
+  }
+
+  setListenerChangeFilter = (callback) => {
+    this._callback.changeFilter = callback;
+    this.element.addEventListener('click', this.#callActionChangeFilter);
+  }
+
+  #callActionChangeFilter = (evt) => {
+    evt.preventDefault();
+    if (evt.target.closest('.trip-filters__filter-label')) {
+      this._callback.changeFilter(evt.target.textContent);
+    }
   }
 }
 
