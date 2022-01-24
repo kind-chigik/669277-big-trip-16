62a54596422321e4e4fcf2484a48cb978f9c8fd3
Инна Русина <rus.work89@yandex.ru>
2022-01-25 00:19:40 +0300

    Теперь в презентеры передаются модели: точек и фильтра

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
 import {renderPosition, renderElement} from './render.js';
 import MenuView from './view/menu-view.js';
-import FilterView from './view/filters-view.js';
-import {generatePoint} from './mock/point.js';
 import TripPresenter from './presenter/trip-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
+import {generatePoint} from './mock/point.js';
+import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 
 const POINT_COUNT = 15;
 
 const points = Array.from({length: POINT_COUNT}, generatePoint);
+const pointsModel = new PointsModel();
+
+pointsModel.points = points;
 
 const navigation = document.querySelector('.trip-controls__navigation');
 const filters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
+const buttonAddNew = document.querySelector('.trip-main__event-add-btn');
 
 const menuInstance = new MenuView();
-const filterInstance = new FilterView();
 
 renderElement(navigation, menuInstance, renderPosition.BEFOREEND);
-renderElement(filters, filterInstance, renderPosition.BEFOREEND);
+const filterModel = new FilterModel();
+const filterPresenter = new FilterPresenter(filters, filterModel);
+filterPresenter.init();
 
-const tripPresenter = new TripPresenter(tripEvents);
-tripPresenter.init(points);
+const tripPresenter = new TripPresenter(tripEvents, pointsModel, buttonAddNew, filterModel);
+tripPresenter.init();
 
