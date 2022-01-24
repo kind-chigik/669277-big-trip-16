d482250ba769e4fc32a2b6d0341989769178d81b
Инна Русина <rus.work89@yandex.ru>
2022-01-24 23:53:58 +0300

    Добавлен файл для хранения констант

---
+++ src/const.js
@@ -0,0 +1,23 @@
+export const typesSort = {
+  BY_DAY: 'sort-day',
+  BY_TIME: 'sort-time',
+  BY_PRICE: 'sort-price',
+};
+
+export const typesUpdate = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+};
+
+export const userAction = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
+};
+
+export const filterType = {
+  EVERYTHING: 'Everything',
+  FUTURE: 'Future',
+  PAST: 'Past',
+};
