6317a35533843c7c905fb2a95ec3b85c970e4b36
Инна Русина <rus.work89@yandex.ru>
2022-01-26 22:45:41 +0300

    Добавлена подслушка на кнопки меню, скорректирована разметка

--- src/view/menu-view.js
+++ src/view/menu-view.js
@@ -1,15 +1,44 @@
 import AbstractView from './abstract-view.js';
+import {itemsMenu} from '../const.js';
 
 const createMenu = () => (
   `<nav class="trip-controls__trip-tabs  trip-tabs">
-  <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-  <a class="trip-tabs__btn" href="#">Stats</a>
+  <a class="trip-tabs__btn  ${itemsMenu.TABLE.toLowerCase()}" href="#">Table</a>
+  <a class="trip-tabs__btn ${itemsMenu.STATS.toLowerCase()}" href="#">Stats</a>
   </nav>`
 );
 
 class MenuView extends AbstractView {
+  #currentItemMenu = itemsMenu.TABLE;
+
   get template() {
     return createMenu();
+  }
+
+  setListenerClickMenu = (callback) => {
+    this._callback.clickMenu = callback;
+    this.element.addEventListener('click', this.#callActionClickMenu);
+  }
+
+  #callActionClickMenu = (evt) => {
+    evt.preventDefault();
+    if (evt.target.closest('.trip-tabs__btn')) {
+      if (evt.target.textContent !== this.#currentItemMenu) {
+        this._callback.clickMenu(evt.target.textContent);
+      }
+    }
+  }
+
+  setActiveMenuItem = (menuItem) => {
+    const itemNotActive = this.element.querySelector(`.${menuItem.toLowerCase()}`);
+    const itemActive = this.element.querySelector(`.${this.#currentItemMenu.toLowerCase()}`);
+
+    if (menuItem !== this.#currentItemMenu) {
+      itemNotActive.classList.add('trip-tabs__btn--active');
+      itemActive.classList.remove('trip-tabs__btn--active');
+    }
+
+    this.#currentItemMenu = menuItem;
   }
 }
 
